7a86ee8ab18314b451f9c289fa5ee157f7db98be
emine-celik <emineclk80@hotmail.com>
2020-11-01 20:31:20 -0600

    Golgelerin guvu adina

--- src/main/java/November.java
+++ src/main/java/November.java
@@ -2,5 +2,6 @@ public class November {
     public static void main(String[] args) {
         System.out.println("Today it was cold");
         System.out.println("Yesterday there was blue moon");
+        System.out.println("Golgelerin gucu adina");
     }
 }
